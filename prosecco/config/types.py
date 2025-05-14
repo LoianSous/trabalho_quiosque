5671fc206391a55e3b09dff6f43f06455ce13a3a
V.Guilherme <v.guilherme.barreto@gmail.com>
2025-05-14 13:03:33 -0400

    Refactor spacing in types.py for improved readability

--- prosecco/config/types.py
+++ prosecco/config/types.py
@@ -10,10 +10,12 @@ class User_state(Enum):
     INACTIVE = "inactive"
     BLOCKED = "blocked"
     
+    
 class File_state(Enum):
     UPLOADED = "uploaded"
     PROCESSED = "processed"
     DELETED = "deleted"
+
 
 class Device_state(Enum):
     ACTIVE = "active"
