36398dfb45ea6d9a86f1716508d111e8b7f7c76a
V.Guilherme <v.guilherme.barreto@gmail.com>
2025-05-27 10:49:34 -0400

    Add group column to Device model and implement to_dict method; update administrative routes for user creation and device retrieval

--- prosecco/models/device.py
+++ prosecco/models/device.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Nullable
 from prosecco.config import db
 from prosecco.config import Device_state
 
@@ -9,8 +10,17 @@ class Device(db.Model):
     ip = db.Column(db.String(128), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     locale = db.Column(db.String(128), nullable=False)
+    group = db.Column(db.String(128), nullable=True, default='geral')
     a_state = db.Column(db.Enum(Device_state, name='device_state'), nullable=False, default=Device_state.ACTIVE)
     dt_created = db.Column(db.Integer, default=db.func.strftime('%s', 'now')) 
     dt_updated = db.Column(db.Integer, default=db.func.strftime('%s', 'now'), onupdate=db.func.strftime('%s', 'now'))
 
     user = db.relationship('User', back_populates='devices')
+
+
+    def to_dict(self):
+        return {
+            'ip': self.ip,
+            'locale': self.locale,
+            'group': self.group
+        }
--- prosecco/routes/administrative_routes.py
+++ prosecco/routes/administrative_routes.py
@@ -1,24 +1,39 @@
+import json
 from flask import Blueprint, request, jsonify
 from prosecco.config import db
-from werkzeug.security import check_password_hash
-from prosecco.models import File_trk, User
-from prosecco.config import User_state
+from werkzeug.security import check_password_hash, generate_password_hash as hash_pass
+from prosecco.config.types import User_type
+from prosecco.models import File_trk, User, Device
+from prosecco.config import User_state, Device_state
 
 adm_route = Blueprint('/adm', __name__)
 
-@adm_route.route('/adm/users')
+@adm_route.route('/adm/users', methods=['GET'])
 def get_all_users():
     all_users = db.session.query(User).filter(User.u_state != User_state.DELETED.value).all()
     users_list = [user.to_dict() for user in all_users]
     return jsonify(users_list)
 
-@adm_route.route('/adm/user/new')
+@adm_route.route('/adm/user/new', methods=['POST'])
 def create_new_user():
     username = request.form.get('name')
     email = request.form.get('email')
+    passphrase = request.form.get('password')
     u_type = request.form.get('u_type')
     
     if db.session.query(User).filter(User.email == email).first():
-        return jsonify(Sucess= False, 'user aready exist')
+        return jsonify(Sucess=False, error='user already exists'), 409
+
+    if not passphrase:
+        return jsonify(success=False, error='Password is required'), 400
+
+    new_user = User(name=username, email=email, passphrase=hash_pass(passphrase), u_type=u_type)
+    
+    return jsonify(sucess=True), 201
     
     
+@adm_route.route('/adm/devices', methods=['GET'])
+def get_all_devices():
+    all_devices = db.session.query(Device).filter(Device.a_state != Device_state.BLOCKED).all()
+    device_list = [device.to_dict() for device in all_devices]
+    return jsonify(device_list)    
